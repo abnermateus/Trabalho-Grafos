d9dc92775195d60b8dda3525896ec4397700e213
IgorLucasAssuncao <igorlucas1217@gmail.com>
2024-11-19 22:07:27 -0300

    Implementa Aresta e Vertice

---
+++ Grafos/Models/Aresta.cs
@@ -0,0 +1,26 @@
+﻿namespace Grafos.Models
+{
+    public enum ClassificacaoAresta
+    {
+        Nenhuma,
+        ArestaArvore,
+        ArestaDeRetorno,
+        ArestaDeAvanco,
+        ArestaDeCruzamento
+    }
+
+    public class Aresta
+    {
+        public Vertice Origem { get; set; }
+        public Vertice Destino { get; set; }
+        public int Peso { get; set; }
+        public ClassificacaoAresta classificacaoAresta { get; set; }
+
+        public Aresta(Vertice origem, Vertice destino, int peso)
+        {
+            Origem = origem;
+            Destino = destino;
+            Peso = peso;
+        }
+    }
+}
---
+++ Grafos/Models/Vertice.cs
@@ -0,0 +1,60 @@
+﻿namespace Grafos.Models
+{
+    public class Vertice
+    {
+        public int Id { get; set; }
+
+        public int Grau { get; private set; }
+
+        public BuscaProfundidade? BuscaProfundidade { get; set; }
+
+        public CaminhoMinimo? CaminhoMinimo { get; set; }
+
+        public Vertice(int id)
+        {
+            Id = id;
+            Grau = 0;
+        }
+
+        public void IncrementaGrau()
+        {
+            Grau++;
+        }
+        public void ResetBuscaEmProfundidade()
+        {
+            BuscaProfundidade = new BuscaProfundidade();
+        }
+
+        public void ResetCaminhoMinimo()
+        {
+            CaminhoMinimo = new CaminhoMinimo();
+        }
+    }
+
+
+    public class BuscaProfundidade
+    {
+        public int DiscoveryTime { get; set; }
+        public int FinishTime { get; set; }
+        public Vertice? Father { get; set; }
+
+        public BuscaProfundidade()
+        {
+            DiscoveryTime = 0;
+            FinishTime = 0;
+            Father = null;
+        }
+    }
+
+    public class CaminhoMinimo
+    {
+        public decimal Distance { get; set; }
+        public Vertice? Father { get; set; }
+
+        public CaminhoMinimo()
+        {
+            Distance = int.MaxValue;
+            Father = null;
+        }
+    }
+}
