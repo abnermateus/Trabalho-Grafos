822f590e69e4972586f725e2d0584b4e5f51265a
IgorLucasAssuncao <igorlucas1217@gmail.com>
2024-11-23 14:54:07 -0300

    Correção classe GrafoMA

--- Grafos/Grafos/MatrizAdjacencia/GrafoMA.cs
+++ Grafos/Grafos/MatrizAdjacencia/GrafoMA.cs
@@ -5,35 +5,39 @@ namespace Grafos.Classes.MatrizAdjacencia
 {
     public class GrafoMA : IGrafo
     {
-        public int[,]? MatrizDeAdjacencia { get; set; }
-        public List<Aresta>? Arestas { get; set; }
+        public Aresta[,]? MatrizDeAdjacencia { get; set; }
         public List<Vertice>? Vertices { get; set; }
 
         public GrafoMA()
         {
             Vertices = null;
-            Arestas = null;
+            MatrizDeAdjacencia = null;
         }
 
-        public GrafoMA(int[,] matriz, List<Aresta> arestas, List<Vertice> vertices)
+        public GrafoMA(List<Aresta> arestas, List<Vertice> vertices)
         {
-            MatrizDeAdjacencia = matriz;
             Vertices = vertices;
-            Arestas = arestas;
+            var matriz = new Aresta[vertices.Count + 1, vertices.Count + 1];
+
+            foreach (var aresta in arestas)
+            {
+                matriz[aresta.Origem.Id, aresta.Destino.Id] = aresta;
+            }
+
+            MatrizDeAdjacencia = matriz;
         }
 
         public GrafoMA InicializaGrafo(List<Vertice> vertices, List<Aresta> arestas)
         {
             Vertices = vertices;
-            Arestas = arestas;
 
             // Matriz é criada com tamanho + 1 para permitir uso direto dos IDs como índices
             // Índice 0 não é utilizado, simplificando o acesso à matriz
-            var matriz = new int[vertices.Count + 1, vertices.Count + 1];
+            var matriz = new Aresta[vertices.Count + 1, vertices.Count + 1];
 
             foreach (var aresta in arestas)
             {
-                matriz[aresta.Origem.Id, aresta.Destino.Id] = aresta.Peso;
+                matriz[aresta.Origem.Id, aresta.Destino.Id] = aresta;
             }
 
             MatrizDeAdjacencia = matriz;
@@ -43,7 +47,7 @@ namespace Grafos.Classes.MatrizAdjacencia
 
         public void Representacao()
         {
-            if (Vertices == null || Arestas == null)
+            if (Vertices == null)
                 throw new InvalidOperationException("Grafo não inicializado");
 
             Console.WriteLine("\nMatriz de Adjacência:");
@@ -71,8 +75,8 @@ namespace Grafos.Classes.MatrizAdjacencia
 
                 for (int j = 1; j <= Vertices.Count; j++)
                 {
-                    var peso = MatrizDeAdjacencia[i, j];
-                    if (peso == 0)
+                    var aresta = MatrizDeAdjacencia[i, j];
+                    if (aresta == null)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.Write(" - ");
@@ -80,7 +84,7 @@ namespace Grafos.Classes.MatrizAdjacencia
                     else
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write($"{peso,3}");
+                        Console.Write($"{aresta.Peso,3}");
                     }
                     Console.ResetColor();
                 }
